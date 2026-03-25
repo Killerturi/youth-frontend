1c582ddf763e6158917c86135f62c2ea53ad36c5
ajoy <test@gmail.com>
2026-03-25 17:15:50 +0530

    add gallery photo

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -11,13 +11,28 @@ import img8 from "../assets/gallery/img8.jpeg";
 import img9 from "../assets/gallery/img9.jpeg";
 import img10 from "../assets/gallery/img10.jpeg";
 import img11 from "../assets/gallery/img11.jpeg";
-import img12 from "../assets/gallery/img12.webp";
+
 import img13 from "../assets/gallery/img13.webp";
 import img14 from "../assets/gallery/img14.webp";
 import img15 from "../assets/gallery/img15.webp";
 import img16 from "../assets/gallery/img16.webp";
 import img17 from "../assets/gallery/img17.webp";
 import img18 from "../assets/gallery/img18.webp";
+import img19 from "../assets/gallery/img19.jpg";
+import img20 from "../assets/gallery/img20.jpg";
+import img21 from "../assets/gallery/img21.jpg";
+import img22 from "../assets/gallery/img22.jpg";
+import img23 from "../assets/gallery/img23.jpg";
+import img24 from "../assets/gallery/img24.jpg";
+import img25 from "../assets/gallery/img25.jpg";
+import img26 from "../assets/gallery/img26.jpg";
+import img27 from "../assets/gallery/img27.jpg";
+import img28 from "../assets/gallery/img28.jpg";
+import img29 from "../assets/gallery/img29.jpg";
+import img30 from "../assets/gallery/img30.jpg";
+import img31 from "../assets/gallery/img31.jpg";
+import img32 from "../assets/gallery/img32.jpg";
+import img33 from "../assets/gallery/img33.jpg";
 
 const galleryImages = [
   { id: 1, src: img1 },
@@ -31,13 +46,26 @@ const galleryImages = [
   { id: 9, src: img9 },
   { id: 10, src: img10 },
   { id: 11, src: img11 },
-  { id: 12, src: img12 },
   { id: 13, src: img13 },
   { id: 14, src: img14 },
   { id: 15, src: img15 },
   { id: 16, src: img16 },
   { id: 17, src: img17 },
-  { id: 18, src: img18 },
+  { id: 19, src: img19 },
+  { id: 20, src: img20 },
+  { id: 21, src: img21 },
+  { id: 22, src: img22 },
+  { id: 23, src: img23 },
+  { id: 24, src: img24 },
+  { id: 25, src: img25 },
+  { id: 26, src: img26 },
+  { id: 27, src: img27 },
+  { id: 28, src: img28 },
+  { id: 29, src: img29 },
+  { id: 30, src: img30 },
+  { id: 31, src: img31 },
+  { id: 32, src: img32 },
+  { id: 33, src: img33 },
 ];
 
 export default galleryImages;
